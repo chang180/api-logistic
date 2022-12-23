98a323a7ccb0f11f025b4f9d12c16e790ed30912
chang180 <chang180@gmail.com>
2022-12-23 17:05:28 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>物流新版單串API 範例程式</title>
+</head>
+
+<body>
+    <h1>藍新金流 物流新版單串API 範例</h1>
+    <ul>
+        <li>
+            <a href="./storeMap/storeMap.php">超商門市地圖選取</a>
+        </li>
+        <li>
+            <a href="./createShipment/createShipment.php">建立物流寄件單</a>
+        </li>
+        <li>
+            <a href="./getShipmentNo/getShipmentNo.php">取寄件單號</a>
+        </li>
+        <li>
+            <a href="./printLabel/printLabel.php">列印寄貨標籤</a>
+        </li>
+    </ul>
+    <!-- <a href="api-logistic.zip" download>下載檔案</a> -->
+</body>
+</html>
